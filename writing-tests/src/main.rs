76021589236cb72977b76cf687260e94cac749dc
Pietro Caselani <pc1992@gmail.com>
2021-05-15 17:06:02 +0200

    Tests and documentation as tests

---
+++ writing-tests/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    println!("{}", is_even(2));
+}
+
+fn is_even(num: i32) -> bool {
+    num % 2 == 0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_true_when_even() {
+        assert!(is_even(2));
+    }
+
+    #[test]
+    fn is_false_when_odd() {
+        assert_eq!(is_even(3), false);
+    }
+}
